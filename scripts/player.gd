7e6bd1d90bdf751477a925b3178ce9abb01d34f3
Ben Kopchains <benkopchains@gmail.com>
2024-05-19 13:02:13 -0400

    animations and fireball
    butts

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,28 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-@onready var arm = $Arm
-@onready var body = $Body
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var speed = 1000;
+
 
 func _physics_process(delta):
-	var mousePos = get_global_mouse_position();
-	arm.look_at(mousePos);
-	if(get_global_mouse_position().x > body.global_position.x):
-		body.flip_h = 0; 
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
 	else:
-		body.flip_h = 1;
-	
-	if(Input.is_action_just_pressed("click")):
-		print('test');
-		var vector = mousePos - global_position;
-		var normalized = vector.normalized();
-		apply_central_force(vector * speed)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
