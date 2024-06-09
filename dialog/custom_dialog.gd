bc71656874196841dc67ea34de8db8e69e535a64
Ben Kopchains <benkopchains@gmail.com>
2024-06-09 13:18:25 -0400

    moved wizard movement code into  fn
    allows us to catch and prevent input during dialog

--- dialog/custom_dialog.gd
+++
@@ -1,6 +0,0 @@
-extends BaseDialogueTestScene;
-
-func _ready():
-	var balloon = load('res://dialog/balloon/balloon.tscn').instantiate();
-	get_tree().current_scene.add_child(balloon);
-	balloon.start(resource, title);
